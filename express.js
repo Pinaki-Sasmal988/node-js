67f04f780fc2f92160ab7abc2782e193cdccbf49
Pinaki-max <spinaki@gmail.com>
2022-11-07 22:51:28 +0530

    Create html page & called

--- express.js
+++ express.js
@@ -1,21 +1,37 @@
 const express=require('express');
+const path=require('path');
 const data=express();
-data.get('',(req,res)=>{
-    //console.log(req.query.name);
-    res.send(`
-    <h3>Welcome ${req.query.name} This is home page</h3>
-    <a href="/about">About</a>
-    `);
-});
-data.get('/about',(req,res)=>{
-    res.send(`
-    <h3>Hello,This is about page</h3>
-    <input type="text" value="${req.query.name}">
-    <button>Click</button>
-    <a href="/">Home</a>
-    `);
-});
-data.get('/contact',(req,res)=>{
-    res.send("Hello,This is contact page");
-});
-data.listen(5000);
+const dirPath=path.join(__dirname,'public');
+// data.get('',(req,res)=>{
+//     //console.log(req.query.name);
+//     res.send(`
+//     <h3>Welcome ${req.query.name} This is home page</h3>
+//     <a href="/about">About</a>
+//     `);
+// });
+// data.get('/about',(req,res)=>{
+//     res.send(`
+//     <h3>Hello,This is about page</h3>
+//     <input type="text" value="${req.query.name}">
+//     <button>Click</button>
+//     <a href="/">Home</a>
+//     `);
+// });
+// data.get('/contact',(req,res)=>{
+//     res.send(`[
+//     {
+//      name:"pinaki",
+//      email:"pinaki@gmail.com"     
+//     },
+//     {
+//         name:"peter",
+//         email:"peter@gmail.com"     
+//        }
+//    ]
+//     `);
+// });
+//
+ data.use(express.static(dirPath));
+
+//console.log(dirPath);
+data.listen(4500);
