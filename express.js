fa0ffad2b3a10bee48ebb2daae9fb8ce03345645
Pinaki-max <spinaki@gmail.com>
2022-11-08 11:51:06 +0530

    Template Eng ejs

--- express.js
+++ express.js
@@ -6,13 +6,21 @@ const dirPath=path.join(__dirname,'public');
  /*How to remove extention name from URL and also set page not found*/ 
 //data.use(express.static(dirPath));
 
-data.get('/home',(req,res)=>{
-    res.sendFile(`${dirPath}/home.html`);
+data.set('view engine','ejs');
+data.get('/profile',(req,res)=>{
+    const value={
+        name:"pinaki",
+        city:"kolkata"
+    }
+    res.render('profile',{value});
 });
-data.get('/about',(req,res)=>{
-    res.sendFile(`${dirPath}/about.html`);
-});
-data.get('*',(req,res)=>{
-    res.sendFile(`${dirPath}/404error.html`);
-});
-data.listen(4500);
+// data.get('/home',(req,res)=>{
+//     res.sendFile(`${dirPath}/home.html`);
+// });
+// data.get('/about',(req,res)=>{
+//     res.sendFile(`${dirPath}/about.html`);
+// });
+// data.get('*',(req,res)=>{
+//     res.sendFile(`${dirPath}/404error.html`);
+// });
+ data.listen(5000);
