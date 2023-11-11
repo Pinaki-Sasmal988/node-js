f0564f7c2328fdad2dd778c4c8477044290fe496
Pinaki-max <spinaki@gmail.com>
2023-11-11 10:39:20 +0530

    update all

--- insert.js
+++ insert.js
@@ -4,13 +4,13 @@ const getConnection=require('./mongodb');
 const insert= async()=>{
  const db=await getConnection();
  const result= await db.insertMany(
-    [{
-        name:'deba',
-        brand:'redmi',
+    {
+        name:'sham',
+        brand:'iqoo',
         price:'15000',
         category:'mobile'
-    },{name:'subhajit',brand:'kgf',price:'12000'}
-]
+    }
+
  );
  if(result.acknowledged){
     console.log("Data inserted");
